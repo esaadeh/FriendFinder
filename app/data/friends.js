4bad9cdf26b4f2e9a79a1a48b62ab1d9e2298779
EKS <saadeh.emad@gmail.com>
2018-12-17 18:43:18 -0800

    Functioning app. General houscleaning and readMe to be done.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,76 @@
-
-
-var friendsArray = [
+var friendsArray = [];
+friendsArray = [
     {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
-          ]
-      }
-  ];
-  
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            '5',
+            '1',
+            '4',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '4',
+            '1'
+        ]
+    },
+    {
+        "name": "TEST",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            '2',
+            '4',
+            '2',
+            '5',
+            '5',
+            '2',
+            '4',
+            '5',
+            '4',
+            '3'
+        ]
+    }
+];
 
-  module.exports = friendsArray;
-  
+
+var name = '';
+var photo;
+var ans1;
+var ans3;
+var ans4;
+var ans5;
+var ans6;
+var ans7;
+var ans8;
+var ans9;
+var ans10;
+var scores = [];
+var obj = {};
+
+for (var i = 0; i < 1; i++) {
+    name = 'n' + i;
+    photo = `https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/${name}.jpg`
+    ans1 = Math.floor((Math.random() * 5) + 1);
+    ans2 = Math.floor((Math.random() * 5) + 1);
+    ans3 = Math.floor((Math.random() * 5) + 1);
+    ans4 = Math.floor((Math.random() * 5) + 1);
+    ans5 = Math.floor((Math.random() * 5) + 1);
+    ans6 = Math.floor((Math.random() * 5) + 1);
+    ans7 = Math.floor((Math.random() * 5) + 1);
+    ans8 = Math.floor((Math.random() * 5) + 1);
+    ans9 = Math.floor((Math.random() * 5) + 1);
+    ans10 = Math.floor((Math.random() * 5) + 1);
+
+    scores = [(ans1).toString(), (ans2).toString(), (ans3).toString(), (ans4).toString(), (ans5).toString(), (ans6).toString(), (ans7).toString(), (ans8).toString(), (ans9).toString(), (ans10).toString()];
+    obj = { name, photo, scores };
+
+    friendsArray.push(obj);
+};
+console.log(friendsArray);
+
+
+
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,35 +8,55 @@ var friends = require("../data/friends");
 //Routing
 
 
-module.exports = function(app) {
+module.exports = function (app) {
 
 
     // Listens for a get request on the '/api/friends' route and responds with a json of the friends objects array
     app.get('/api/friends', function (req, res) {
         res.json(friends);
+        // console.log(res);
+        // console.log(friends);
     });
 
     // Listens for  post request on the '/api/friends' route...
     app.post('/api/friends', function (req, res) {
+        // console.log(friends[0].scores);
+        // console.log(friends[0].socres);
 
         var newPost = req.body;
+        // var newScores = req.body.scores;
+        console.log(newPost.scores);
+        // console.log(parseInt(friends[0].scores[0]));
+        // console.log((newPost.scores[0]));
+        // console.log(parseInt(friends[0].scores[0]) + parseInt(newPost.scores[0]));
+
+        // console.log(friends.length);
+        // console.log(friends[0].scores.length);
+        // console.log(newPost.scores.length);
+        
+        var match = 50;
+        var matchIndex = 0;
+        for (var i = 0; i < friends.length; i++) {
+             var delta = 0;
+            for (var y = 0; y < newPost.scores.length; y++) {
+                delta += Math.abs((friends[i].scores[y]) - newPost.scores[y]);
+                console.log(delta);
+            }
+
+            if (delta < match) {
+                match = delta;
+                matchIndex = i;
+                
+                console.log('match: ' + match);
+                console.log('Index: ' +matchIndex);
+            }
+        }
 
         friends.push(newPost);
         // res.json(true);
 
-        // // Using a RegEx Pattern to remove spaces from newCharacter
-        // // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-        // newPost.name = newPost.name.replace(/\s+/g, "").toLowerCase();
-        // newPost.photo = newPost.photo.replace(/\s+/g, "").toLowerCase();
-        // newPost.phone = newPost.name.replace(/\s+/g, "").toLowerCase();
-        // newPost.email = newPost.name.replace(/\s+/g, "").toLowerCase();
-        // newPost.uniqueID = newPost.name.replace(/\s+/g, "").toLowerCase();
-        // console.log(newPost);
-
-        // reservations.push(newPost);
-
-        res.json(newPost);
-
+        //Output logic here for the json(reply)
+        res.json(friends[matchIndex]);
     });
 
 };
